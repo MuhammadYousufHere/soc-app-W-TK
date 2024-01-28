dd5190dfbf02062b90382a1826fa954e6f3747a1
MuhammadYousufHere <khanyousufmy80@gmail.com>
2024-01-28 22:35:03 +0500

    chore: add default url

--- client/src/redux/api/utils.js
+++ client/src/redux/api/utils.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_API_URL;
+const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000';
+console.log({BASE_URL})
 const ADMIN_URL = `${BASE_URL}/admin`;
 
 const authInterceptor = (req) => {
